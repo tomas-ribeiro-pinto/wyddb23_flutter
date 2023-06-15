8f5f6aab24084a69d78db4aac3f5cc769d7bf344
tomas-ribeiro-pinto <morato.toms@outlook.com>
2023-06-15 09:24:55 +0100

    home_page design

--- lib/Activities/home_activity.dart
+++ lib/Activities/home_activity.dart
@@ -34,7 +34,8 @@ class _HomeActivityState extends State<HomeActivity> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: pages[pageIndex],
+      extendBody: true,
+      body: SingleChildScrollView(physics: ClampingScrollPhysics(),child: pages[pageIndex]),
       backgroundColor: Colors.white,
     
       // set the bottom navigation bar
@@ -54,129 +55,131 @@ class _HomeActivityState extends State<HomeActivity> {
       ),
       // Includes a SafeArea for IOS devices with notch
       child: SafeArea(
-        child: Container(
+        child: SizedBox(
           height: 65,
-          decoration: const BoxDecoration(
-            color: Color(0xFF028744),
-            borderRadius: BorderRadius.only(
-              topLeft: Radius.circular(20),
-              topRight: Radius.circular(20),
+          child: Container(
+            decoration: const BoxDecoration(
+              color: Color(0xFF028744),
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(20),
+                topRight: Radius.circular(20),
+              ),
             ),
-          ),
-          child: OverflowBox(
-            maxHeight: 110,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                IconButton(
-                  enableFeedback: false,
-                  onPressed: () {
-                    setState(() {
-                      pageIndex = PAGE1_index;
-                    });
-                  },
-                  icon: pageIndex == PAGE1_index
-                      ? const Icon(
-                          Icons.home_filled,
-                          color: Colors.white,
-                          size: 35,
-                        )
-                      : const Icon(
-                          Icons.home_outlined,
-                          color: Colors.white,
-                          size: 35,
-                        ),
-                ),
-                IconButton(
-                  enableFeedback: false,
-                  onPressed: () {
-                    setState(() {
-                      pageIndex = PAGE2_index;
-                    });
-                  },
-                  icon: pageIndex == PAGE2_index
-                      ? const Icon(
-                          Icons.work_rounded,
-                          color: Colors.white,
-                          size: 35,
-                        )
-                      : const Icon(
-                          Icons.work_outline_outlined,
-                          color: Colors.white,
-                          size: 35,
-                        ),
-                ),
-                Container(
-                  margin: const EdgeInsets.only(bottom: 35),
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    border: Border.all(
-                      color: const Color.fromARGB(255, 194, 194, 194),
-                      width: 0.3,
-                    ),
-                    borderRadius: BorderRadius.circular(50),
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.grey.withOpacity(0.3),
-                        spreadRadius: 2,
-                        blurRadius: 40,
-                        offset: const Offset(0, 3), // changes position of shadow
-                      ),
-                    ],
-                  ),
-                  child: IconButton(
+            child: OverflowBox(
+              maxHeight: 110,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  IconButton(
                     enableFeedback: false,
                     onPressed: () {
                       setState(() {
-                        pageIndex = HOMEPAGE_index;
+                        pageIndex = PAGE1_index;
                       });
                     },
-                    icon: Image.asset(
-                      'assets/images/wyd-logo-cor.png',
-                      fit: BoxFit.fill,
-                      height: 70,
+                    icon: pageIndex == PAGE1_index
+                        ? const Icon(
+                            Icons.home_filled,
+                            color: Colors.white,
+                            size: 35,
+                          )
+                        : const Icon(
+                            Icons.home_outlined,
+                            color: Colors.white,
+                            size: 35,
+                          ),
+                  ),
+                  IconButton(
+                    enableFeedback: false,
+                    onPressed: () {
+                      setState(() {
+                        pageIndex = PAGE2_index;
+                      });
+                    },
+                    icon: pageIndex == PAGE2_index
+                        ? const Icon(
+                            Icons.work_rounded,
+                            color: Colors.white,
+                            size: 35,
+                          )
+                        : const Icon(
+                            Icons.work_outline_outlined,
+                            color: Colors.white,
+                            size: 35,
+                          ),
+                  ),
+                  Container(
+                    margin: const EdgeInsets.only(bottom: 35),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      border: Border.all(
+                        color: const Color.fromARGB(255, 194, 194, 194),
+                        width: 0.3,
+                      ),
+                      borderRadius: BorderRadius.circular(50),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey.withOpacity(0.3),
+                          spreadRadius: 2,
+                          blurRadius: 40,
+                          offset: const Offset(0, 3), // changes position of shadow
+                        ),
+                      ],
+                    ),
+                    child: IconButton(
+                      enableFeedback: false,
+                      onPressed: () {
+                        setState(() {
+                          pageIndex = HOMEPAGE_index;
+                        });
+                      },
+                      icon: Image.asset(
+                        'assets/images/wyd-logo-cor.png',
+                        fit: BoxFit.fill,
+                        height: 70,
+                      ),
                     ),
                   ),
-                ),
-                IconButton(
-                  enableFeedback: false,
-                  onPressed: () {
-                    setState(() {
-                      pageIndex = PAGE3_index;
-                    });
-                  },
-                  icon: pageIndex == PAGE3_index
-                      ? const Icon(
-                          Icons.access_alarm_outlined,
-                          color: Colors.white,
-                          size: 35,
-                        )
-                      : const Icon(
-                          Icons.person_outline,
-                          color: Colors.white,
-                          size: 35,
-                        ),
-                ),
-                IconButton(
-                  enableFeedback: false,
-                  onPressed: () {
-                    setState(() {
-                      pageIndex = PAGE4_index;
-                    });
-                  },
-                  icon: pageIndex == PAGE4_index
-                      ? const Icon(
-                          Icons.person,
-                          color: Colors.white,
-                          size: 35,
-                        )
-                      : const Icon(
-                          Icons.person_outline,
-                          color: Colors.white,
-                          size: 35,
-                        ),
-                ),
-              ],
+                  IconButton(
+                    enableFeedback: false,
+                    onPressed: () {
+                      setState(() {
+                        pageIndex = PAGE3_index;
+                      });
+                    },
+                    icon: pageIndex == PAGE3_index
+                        ? const Icon(
+                            Icons.access_alarm_outlined,
+                            color: Colors.white,
+                            size: 35,
+                          )
+                        : const Icon(
+                            Icons.person_outline,
+                            color: Colors.white,
+                            size: 35,
+                          ),
+                  ),
+                  IconButton(
+                    enableFeedback: false,
+                    onPressed: () {
+                      setState(() {
+                        pageIndex = PAGE4_index;
+                      });
+                    },
+                    icon: pageIndex == PAGE4_index
+                        ? const Icon(
+                            Icons.person,
+                            color: Colors.white,
+                            size: 35,
+                          )
+                        : const Icon(
+                            Icons.person_outline,
+                            color: Colors.white,
+                            size: 35,
+                          ),
+                  ),
+                ],
+              ),
             ),
           ),
         ),
--- lib/NavigationPages/home_page.dart
+++ lib/NavigationPages/home_page.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
@@ -23,76 +22,273 @@ class _HomePageState extends State<HomePage> {
     return AnnotatedRegion<SystemUiOverlayStyle>(
       // Set Status Bar Text Color
       value: SystemUiOverlayStyle.light,
-      child: Stack(
+      child: Column(
         children: [
-          Container(
-            decoration: const BoxDecoration(
-              image: DecorationImage(
+          Stack(
+            children: [
+              const Image(
                 image: AssetImage("assets/images/wyd-home-green.jpg"),
-                fit: BoxFit.fitWidth,
-                alignment: Alignment.topLeft,
               ),
-            ),
-            child: Padding(
-              padding: const EdgeInsets.all(30.0),
-              child: Container(
-                margin: const EdgeInsets.only(top: 160),
-                decoration: const BoxDecoration(
-                  image: DecorationImage(
+              Padding(
+                padding: const EdgeInsets.all(24.0),
+                child: Container(
+                  margin: const EdgeInsets.only(top: 160),
+                  child: const Image(
                     image: AssetImage("assets/images/home-pic.png"),
-                    fit: BoxFit.contain,
-                    alignment: Alignment.topLeft,
                   ),
                 ),
               ),
-            ),
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: [
-              Transform.scale(
-                scale: 0.65,
-                child: Container(
-                  margin: const EdgeInsets.only(top:45, right: 12),
+              Container(
+                alignment: Alignment.topRight,
+                margin: const EdgeInsets.only(top: 45, right: 12),
+                child: Transform.scale(
+                  scale: 0.65,
                   child: Container(
                     decoration: BoxDecoration(
-                      border: Border.all(
-                        color: const Color.fromARGB(255, 194, 194, 194),
-                        width: 1,
-                      ),
-                      borderRadius: BorderRadius.circular(15)
-                    ),
-                    child: FloatingActionButton( 
+                        border: Border.all(
+                          color: const Color.fromARGB(255, 194, 194, 194),
+                          width: 1,
+                        ),
+                        borderRadius: BorderRadius.circular(15)),
+                    child: FloatingActionButton(
                       backgroundColor: const Color.fromARGB(255, 35, 35, 35),
                       foregroundColor: Colors.white,
-                      onPressed:() {},
+                      onPressed: () {},
                       child: const Text(
                         "EN",
                         style: TextStyle(
-                        fontWeight: FontWeight.bold, 
-                        color: Colors.white,
-                        fontSize: 25,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                          fontSize: 25,
                         ),
                       ),
                     ),
                   ),
                 ),
               ),
+              Container(
+                margin: const EdgeInsets.only(top: 90),
+                child: const Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        "Hello, Lisboa",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                          fontSize: 33,
+                        ),
+                      )
+                    ]),
+              ),
+              Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                    margin: EdgeInsets.only(top:140),
+                    height: 30,
+                    width: 90,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(18),
+                      color: const Color.fromARGB(255, 216, 44, 32),
+                    ),
+                    child: Row(
+                      children: [
+                        Container(
+                          margin: EdgeInsets.only(left:5),
+                          height: 20,
+                          width: 20,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(18),
+                            color: Colors.white
+                          ),
+                          child: Image(
+                            image: AssetImage("assets/images/weather-sun.png"),
+                          ),
+                        ),
+                        Container(
+                          margin: EdgeInsets.only(left:5),
+                          child: Text(
+                            "25 ºC",
+                            textAlign: TextAlign.right,
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 18,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ]),
             ],
           ),
-          Container(
-            margin: const EdgeInsets.only(top:90),
-            child: const Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [Text(
-                "Hello, Lisboa",
-                style: TextStyle(
-                  fontWeight: FontWeight.bold, 
-                  color: Colors.white,
-                  fontSize: 33,
-                ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Column(
+                children: [
+                  IconButton(
+                    onPressed: () {},
+                    enableFeedback: false,
+                    icon: Image.asset(
+                      'assets/images/highlight-lisbon.png',
+                      fit: BoxFit.fill,
+                      height: 80,
+                    ),
+                  ),
+                  Text(
+                    "Agenda",
+                    style: TextStyle(
+                      fontSize: 15,
+                    ),
+                  ),
+                ],
               ),
-            ]),
+              Column(
+                children: [
+                  IconButton(
+                    onPressed: () {},
+                    enableFeedback: false,
+                    icon: Image.asset(
+                      'assets/images/highlight-world.png',
+                      fit: BoxFit.fill,
+                      height: 80,
+                    ),
+                  ),
+                  Text(
+                    "Map",
+                    style: TextStyle(
+                      fontSize: 15,
+                    ),
+                  ),
+                ],
+              ),
+              Column(
+                children: [
+                  IconButton(
+                    onPressed: () {},
+                    enableFeedback: false,
+                    icon: Image.asset(
+                      'assets/images/highlight-cross.png',
+                      fit: BoxFit.fill,
+                      height: 80,
+                    ),
+                  ),
+                  Text(
+                    "Hymn",
+                    style: TextStyle(
+                      fontSize: 15,
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+          SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            child: Container(
+              margin: EdgeInsets.only(top: 30),
+              child: Row(
+                children: [
+                  Container(
+                    margin: EdgeInsets.only(left: 15, right: 8),
+                    height: 100,
+                    width: 100,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(18),
+                      color: const Color.fromARGB(255, 216, 44, 32),
+                    ),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Container(
+                          margin: EdgeInsets.only(top: 8),
+                          child: Text(
+                            "🙏",
+                            style: TextStyle(
+                              fontSize: 35,
+                            ),
+                          ),
+                        ),
+                        Text(
+                          "SYM Day",
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
+                            fontSize: 20,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(right: 8),
+                    height: 100,
+                    width: 100,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(18),
+                      color: Color.fromARGB(255, 25, 20, 20),
+                    ),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Container(
+                          margin: EdgeInsets.only(top: 8),
+                          child: Text(
+                            "📝",
+                            style: TextStyle(
+                              fontSize: 35,
+                            ),
+                          ),
+                        ),
+                        Text(
+                          "Notes",
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
+                            fontSize: 20,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(right: 8),
+                    height: 100,
+                    width: 100,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(18),
+                      color: Color.fromARGB(115, 13, 13, 13),
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(right: 8),
+                    height: 100,
+                    width: 100,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(18),
+                      color: Color.fromARGB(115, 13, 13, 13),
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(right: 8),
+                    height: 100,
+                    width: 100,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(18),
+                      color: Color.fromARGB(115, 13, 13, 13),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Container(
+            height: 100,
+            color: Colors.transparent,
           ),
         ],
       ),
